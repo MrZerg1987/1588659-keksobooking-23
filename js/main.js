74280025dbc1280a3286b277f42e0071e5dc3ec8
Kozin Sergey <6303420@mail.ru>
2021-06-04 23:57:45 +0300

    Домашнее задание №4. Генерация данных

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Функция, возвращающая рандомное положительное число.
 
-const getRandomNumber = function(from, to, roundTo = 0) {
+const getRandomNumber = (from, to, roundTo = 0) => {
   const num = from + Math.random() * (to - from);
   if (from >= 0 && from < to) {
     return roundTo > 0 ? num.toFixed(roundTo) : Math.round(num);
@@ -8,4 +8,80 @@ const getRandomNumber = function(from, to, roundTo = 0) {
   return 'Заданы неверные параметры';
 };
 
-getRandomNumber(0, 3, 2);
+// Функция, возвращающая рандомный элемент массива
+
+const getRandomArrayElement = (elements) => elements[Math.round(Math.random() * (elements.length - 1))];
+
+const USERS = [
+  'user01',
+  'user02',
+  'user03',
+  'user04',
+  'user05',
+  'user06',
+  'user07',
+  'user08',
+];
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const TIME = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const LAT = getRandomNumber(35.65000, 35.70000, 5);
+const LNG = getRandomNumber(139.70000, 139.80000, 5);
+
+const OBJECT_COUNT = 10;
+
+const obj = {
+  author: {
+    avatar: `img/avatars/${getRandomArrayElement(USERS)}.png`,
+  },
+  offer: {
+    title: 'Внимание! Горячее предложение! Успейте оформить заказ!',
+    address: `${LAT}, ${LNG}`,
+    price: `$${getRandomNumber(100, 1000)}`,
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomNumber(1, 5),
+    guests: getRandomNumber(1, 10),
+    checkin: getRandomArrayElement(TIME),
+    checkout: getRandomArrayElement(TIME),
+    features:  new Set (new Array(getRandomNumber(1, 6)).fill(null).map(() => getRandomArrayElement(FEATURES))),
+    description: 'Самое лучшее предложение по мнению пользователей нашего сайта!',
+    photos: new Set (new Array(getRandomNumber(1, 3)).fill(null).map(() => getRandomArrayElement(PHOTOS))),
+  },
+  location: {
+    lat: LAT,
+    lng: LNG,
+  },
+};
+
+const createObj = () => obj;
+
+const similarObjects = new Array(OBJECT_COUNT).fill(null).map(() => createObj());
+
+similarObjects();
