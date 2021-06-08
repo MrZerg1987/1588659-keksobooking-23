f454a68a157b52e5ee070f568a2ec198994d278e
Kozin Sergey <6303420@mail.ru>
2021-06-08 21:14:10 +0300

    Домашнее задание №5. Модуляция. Правки

--- js/main.js
+++ js/main.js
@@ -3,4 +3,3 @@ import {createObj} from './util.js';
 
 const createSimilarObjects = () => new Array(OBJECT_COUNT).fill(null).map(() => createObj());
 createSimilarObjects();
-Домашнее задание №4. Генерация данных. Правки +
